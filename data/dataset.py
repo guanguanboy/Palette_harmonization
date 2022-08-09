a56539535f4aa38c4cae8d26b926d1d68613d8ed
liguanlin <517445163@qq.com>
2022-08-09 10:32:19 +0800

    add dataset class to read RealHM

--- data/dataset.py
+++ data/dataset.py
@@ -362,3 +362,73 @@ class RestorationTestDataset(data.Dataset):
 
     def __len__(self):
         return len(self.imgs)
+
+
+
+class SSHarmonizationTestDataset(data.Dataset):
+    def __init__(self, data_root, mask_config={}, data_len=-1, image_size=[256, 256], loader=pil_loader):
+        # data_root的样例../RealHM
+        self.image_small_list = []
+        for root, dirs, files in os.walk(data_root  + "/vendor_testing_1/"):
+            for file in files:
+                if "small" in file:
+                    self.image_small_list.append(os.path.join(root, file))
+        for root, dirs, files in os.walk(data_root  + "/vendor_testing_2/"):
+            for file in files:
+                if "small" in file:
+                    self.image_small_list.append(os.path.join(root, file))
+                    
+        for root, dirs, files in os.walk(data_root  + "/vendor_testing_3/"):
+            for file in files:
+                if "small" in file:
+                    self.image_small_list.append(os.path.join(root, file))
+        
+        
+        self.tfs = transforms.Compose([
+                transforms.Resize((image_size[0], image_size[1])),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
+        ])
+        self.loader = loader
+        self.image_size = image_size
+        
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
+        ])
+
+    def __getitem__(self, index):
+        ret = {}
+        path = self.image_small_list[index]
+
+        comp_path = path.replace("_small.jpg", "_composite_noise25.jpg")
+        mask_path = path.replace("_small.jpg", "_mask.jpg")
+        target_path = path.replace("_small.jpg", "_gt.jpg")
+
+        comp = Image.open(comp_path).convert('RGB')
+        real = Image.open(target_path).convert('RGB')
+        mask = Image.open(mask_path).convert('1')
+
+        comp = tf.resize(comp, [self.image_size[0], self.image_size[1]])
+        mask = tf.resize(mask, [self.image_size[0], self.image_size[1]])
+        #mask = tf.resize(mask, [224, 224]) #对MAE训练，需要将这里修改为224,224
+        real = tf.resize(real,[self.image_size[0],self.image_size[1]])
+
+        #apply the same transform to composite and real images
+        comp = self.transform(comp)
+        #mask = self.mask_transform(mask)
+        mask = tf.to_tensor(mask)
+
+        real = self.transform(real)
+
+        ret['gt_image'] = real
+        ret['cond_image'] = comp
+        ret['mask'] = mask
+        ret['path'] = path.rsplit("/")[-1]
+
+        return ret
+
+    def __len__(self):
+        return len(self.image_small_list)
+
+
